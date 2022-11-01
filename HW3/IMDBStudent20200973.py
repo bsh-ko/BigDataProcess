7df349bff3192bac87cceb5ada1843e77ea43d45
bsh-ko <sunny3872@naver.com>
2022-11-01 19:28:12 +0900

    add HW3 code

---
+++ HW3/IMDBStudent20200973.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+import sys
+
+inputFile = sys.argv[1]
+outputFile = sys.argv[2]
+
+G = {}
+with open(inputFile, "rt") as f:
+	for r in f:
+		movie = r.split("::")
+		genre = movie[2].split("|")
+
+		for g in genre:
+			if g in G:
+				G[g] += 1
+			else:
+				G[g] = 1
+
+with open(outputFile, "wt") as f:
+	for r in G:
+		f.write("%s %d" % (g, G[g]))

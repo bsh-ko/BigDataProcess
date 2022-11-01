aee6314ee79ba6ea67a0ae913607051e114bb3c9
bsh-ko <sunny3872@naver.com>
2022-11-02 00:36:35 +0900

    add HW3 code

--- HW3/UBERStudent20200973.py
+++ HW3/UBERStudent20200973.py
@@ -6,27 +6,27 @@ from datetime import datetime, date
 inputFile = sys.argv[1]
 outputFile = sys.argv[2]
 
-days = ['MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT', 'SUN']
-u_dic1 = dict()
-u_dic2 = dict()
+days = ["MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"]
+d1 = dict()
+d2 = dict()
 
 with open(inputFile, "rt") as f:
 	for row in f:
 		uber = row.split(",")
 		uber[-1] = uber[-1].split("\n")[0]
-		uberDate = uber[1].split("/")
-		uber[1] = days[date(int(day[2]), int(day[0]), int(day[1])).weekday()]
+		uberDay = uber[1].split("/")
+		uber[1] = days[date(int(uberDay[2]), int(uberDay[0]), int(uberDay[1])).weekday()]
 
 		str = uber[0] + "," + uber[1]
-		if key not in u_dic1:
-			u_dic1[key] = int(uber[2])	
-			u_dic2[key] = int(uber[3])	
+		if key not in d1:
+			d1[key] = int(uber[2])	
+			d2[key] = int(uber[3])	
 
 		else:
-			u_dic1[key] += int(uber[2])	
-			u_dic2[key] += int(uber[3])	
+			d1[key] += int(uber[2])	
+			d2[key] += int(uber[3])	
 
-string = u_dic1.keys()
+string = d1.keys()
 
 with open(outputFile, "wt") as f:
 	for k in string:
